7a5580aec40dc1e411dfdac35ad91a924830fc01
santiagosod <poised691@gmail.com>
2022-11-03 23:08:28 -0500

    add swagger

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const cors = require("cors")
 const db = require("./tools/database")
+const swaggerUi = require("swagger-ui-express")
 
 //? Files
 const { port } = require('./config')
+const swaggerDoc = require("../swagger.json")
 
 //* Routes
 const userRouter = require("./users/users.router")
@@ -46,6 +48,7 @@ app.get('/', (req, res) => {
     })
 })
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.serve(swaggerDoc))
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/categories", categoryRouter)
